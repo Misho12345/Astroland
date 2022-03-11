a3278bab3a5c01bb391fbbc24849a4ffa4caeba7
Mishu <54718081+Misho12345@users.noreply.github.com>
2022-03-11 15:29:47 +0200

    nice moving

--- Script2.js
+++ Script2.js
@@ -1,9 +1,25 @@
-var player = new Player(canvas.width / 2 - 50, 100, 100);
-var planet = new Planet(canvas.width * 2);
+let planet = new Planet(canvas.width * 2);
+let player = new Player(100, 100);
+
+let cap = Math.PI / 20;
+let speed = Math.PI / 720;
 
 function Update() {
-    if (isKeyPressed[39] || isKeyPressed[68]) player.angle += Math.PI / 1440;
-    if (isKeyPressed[37] || isKeyPressed[65]) player.angle -= Math.PI / 1440;
+    if (isKeyPressed[39] || isKeyPressed[68]) {
+        if (player.angle > player.defA + cap) {
+            planet.angle -= speed;
+            player.defA = player.angle - cap;
+        }
+        player.angle += speed;
+    }
+    if (isKeyPressed[37] || isKeyPressed[65]) {
+        if (player.angle < player.defA - cap) {
+            planet.angle += speed;
+            player.defA = player.angle + cap;
+        }
+        player.angle -= speed;
+    }
+
     canvas.width = window.innerWidth - 20;
     canvas.height = window.innerHeight - 20;
     
@@ -11,8 +27,13 @@ function Update() {
 
 function Draw() {
     ctx.drawImage(document.getElementById("fon"), 0, 0, canvas.width, canvas.height);
-    if (!isKeyPressed[77]) { ctx.translate(0, planet.diameter - canvas.width+250); } else { ctx.scale(0.2, 0.2); ctx.translate(canvas.width + planet.diameter / 2, 0 + planet.diameter / 2); };
+
+    if (!isKeyPressed[77]) ctx.translate(0, planet.diameter - canvas.width + 250);
+    else {
+        ctx.scale(0.2, 0.2);
+        ctx.translate(canvas.width + planet.diameter / 2, 0 + planet.diameter / 2);
+    }
+
     planet.draw();
     player.draw();
-    
 }
--- nesho.js
+++ nesho.js
@@ -9,6 +9,14 @@ for (let i = 0; i < 256; isKeyPressed[i++] = 0);
 canvas.width = window.innerWidth - 20;
 canvas.height = window.innerHeight - 20;
 
+let elem = document.documentElement;
+
+function openFullscreen() {
+    if (elem.requestFullscreen) elem.requestFullscreen();
+    else if (elem.webkitRequestFullscreen) elem.webkitRequestFullscreen();
+    else if (elem.msRequestFullscreen) elem.msRequestFullscreen();
+}
+
 function init() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -17,6 +25,7 @@ function init() {
 
     setTimeout(init, 10);
 }
+
 enemyClasses.push(class smallEnemy {
     constructor(x, y, type) {
         this.x = x;
@@ -43,6 +52,7 @@ enemyClasses.push(class smallEnemy {
                 ));
     }
 })
+
 class Bullet {
     constructor(x_, y_, dX_, dY_, color_, r_, speed_, demage_) {
         this.x = x_;
@@ -67,33 +77,30 @@ class Bullet {
         context.fill();
     }
 }
+
 class Player {
-    constructor(x, width, height) {
-        this.defX = x;
-        this.defY = 375;
-
-        this.x = this.defX;
-        this.y = this.defXY;
-
+    constructor(width, height) {
+        this.x, this.y;
         this.width = width;
         this.height = height;
 
         this.angle = Math.PI / -2;
+        this.h = planet.diameter / 2 + this.height / 4;
+
+        this.defA = this.angle;
     }
 
     draw() {
-        this.x = this.defX + Math.cos(this.angle) * planet.diameter / 2;
-        this.y = this.defY + (Math.sin(this.angle) + 1) * planet.diameter / 2;
+        this.x = Math.cos(this.angle) * this.h + canvas.width / 2 - this.width / 2;
+        this.y = Math.sin(this.angle) * this.h + canvas.height / 2 - this.height / 2;
 
         ctx.save();
 
         ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
-        ctx.rotate(Math.PI / 2 * ((this.x + this.width / 2) / planet.diameter - 0.25));
-        ctx.translate(-(this.x + this.width / 2), -(this.y + this.height / 2));
+        ctx.rotate(this.angle + Math.PI / 2);
+        ctx.translate(-this.x - this.width / 2, -this.y - this.height / 2);
 
         ctx.drawImage(document.getElementById("player"), this.x, this.y, this.width, this.height);
-
-        ctx.rotate(-(Math.PI / 2 * ((this.x + this.width / 2) / planet.diameter - 0.25)));
 
         ctx.restore();
     }
@@ -101,14 +108,21 @@ class Player {
 
 class Planet {
     constructor(diameter) {
-        this.x = -diameter / 4;
         this.diameter = diameter;
-        this.rotation = 0;
+        this.angle = Math.PI / -2;
+
+        this.x, this.y;
     }
 
     draw() {
-       ctx.drawImage(document.getElementById("planet"), canvas.width / 2 - this.diameter / 2, canvas.height / 2 - this.diameter / 2, this.diameter, this.diameter);
+        this.x = canvas.width / 2 - this.diameter / 2;
+        this.y = canvas.height / 2 - this.diameter / 2;
 
+        ctx.translate(this.x + this.diameter / 2, this.y + this.diameter / 2);
+        ctx.rotate(this.angle + Math.PI / 2);
+        ctx.translate(-this.x - this.diameter / 2, -this.y - this.diameter / 2);
+
+        ctx.drawImage(document.getElementById("planet"), this.x, this.y, this.diameter, this.diameter);
     }
 }
 
